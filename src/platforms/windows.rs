c5fdc866a12a6733b0ca48af9c69343140b15073
Steve Lau <stevelauc@outlook.com>
2025-05-20 15:45:12 +0800

    foo:

--- src/platforms/windows.rs
+++ src/platforms/windows.rs
@@ -335,6 +335,49 @@ impl AppTrait for App {
     }
 }
 
+use winreg::enums::*;
+use winreg::RegKey;
+
+fn list_installed_apps() -> Result<(), Box<dyn std::error::Error>> {
+    let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
+    
+    // 64-bit apps
+    let uninstall_64 = hklm.open_subkey_with_flags(
+        "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall",
+        KEY_READ | KEY_WOW64_64KEY,
+    )?;
+
+    // 32-bit apps (on 64-bit Windows)
+    let uninstall_32 = hklm.open_subkey_with_flags(
+        "SOFTWARE\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall",
+        KEY_READ | KEY_WOW64_32KEY,
+    )?;
+
+    // Current user apps
+    let hkcu = RegKey::predef(HKEY_CURRENT_USER);
+    let uninstall_user = hkcu.open_subkey_with_flags(
+        "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall",
+        KEY_READ,
+    )?;
+
+    for key in [uninstall_64, uninstall_32, uninstall_user] {
+        for subkey_name in key.enum_keys() {
+            let subkey_name = subkey_name?;
+            if let Ok(subkey) = key.open_subkey(&subkey_name) {
+                if let Ok(display_name) = subkey.get_value::<String>("DisplayName") {
+                    let system_component: u32 = subkey.get_value("SystemComponent").unwrap_or(0);
+                    if system_component != 1 {
+                        println!("{}", display_name);
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -356,5 +399,10 @@ mod tests {
             path.to_string_lossy().to_lowercase(),
             "c:\\windows\\system32\\mstsc.exe"
         );
+    }
+
+    #[test]
+    fn test_foobar() {
+        list_installed_apps(); 
     }
 }
